e33e7d8e9d52d17d9d5bcaac743eaa053c34246a
Philip Waller <p.waller@uq.edu.au>
2017-09-08 18:16:06 +1000

    case 5

--- src/homework003/calc.js
+++ src/homework003/calc.js
@@ -4,7 +4,7 @@
 
 $('#clearButton').click(function () {
     $('#display').val('');
-    sessionStorage.clear();
+    reset();
 });
 
 $('.digit').click(function () {
@@ -15,31 +15,27 @@ $('.digit').click(function () {
 });
 
 $('.operator').click(function () {
-    var stack = sessionStorage.getItem('stack');
-    stack = (stack) ? JSON.parse(stack) : [];
-    stack.unshift( parseInt($('#display').val()) );
-    stack.unshift( $(this).text() );
-    sessionStorage.setItem('stack', JSON.stringify(stack));
-    sessionStorage.setItem('is_new', true);
-
-    $('#output').text( 'stack: ' + stack );
+    var result = calc();
+    reset( result + $(this).val() );
 });
 
 $('#equalsButton').click(function () {
-    var stack = sessionStorage.getItem('stack');
-    stack = (stack) ? JSON.parse(stack) : [];
-    stack.unshift( parseInt($('#display').val()) );
-
-    var val1 = stack.shift();
-    var op = stack.shift();
-    var val2 = stack.shift();
-
-    var result = val2 + val1;
-    stack.unshift( result );
-    sessionStorage.setItem('stack', JSON.stringify(stack));
-    sessionStorage.setItem('is_new', true);
-
-    $('#display').val( val1 + val2 );
-    $('#output').text( 'stack: ' + stack );
+    calc();
+    reset();
 });
+
+var calc = function() {
+    var eqn = sessionStorage.getItem('equation');
+    var display = $('#display');
+    var result = eval( eqn + display.val() );
+    display.val( result );
+    return result;
+};
+
+var reset = function (eqn) {
+    sessionStorage.clear();
+    sessionStorage.setItem('is_new', true);
+    sessionStorage.setItem('equation', eqn ? eqn : '');
+};
+
 
